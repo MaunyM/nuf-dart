abab0471f00657705df3077a67680fa6e4b34fca
Matthieu <matthieu.mauny@gmail.com>
2025-01-27 16:49:20 +0100

    cleaning

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,14 +3,7 @@ import { useRouter } from "next/router";
 import { useEffect } from "react";
 import { useAuth } from "react-oidc-context";
 
-const signOutRedirect = () => {
-  const clientId = "1cusrvvn01cc7dq61lk8g7mr1a";
-  const logoutUri = "http://localhost:3000";
-  const cognitoDomain = "https://login.larus.fr";
-  window.location.href = `${cognitoDomain}/logout?client_id=${clientId}&logout_uri=${encodeURIComponent(
-    logoutUri
-  )}`;
-};
+
 
 export default function Page() {
   const router = useRouter();
